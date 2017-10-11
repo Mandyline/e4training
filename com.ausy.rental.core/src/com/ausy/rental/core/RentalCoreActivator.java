420ffa51740b76387bb3338a132e735c03c8dad3
formation <formation@formation-Aspire-X3995>
2017-10-11 10:20:20 +0200

    mise à dispo agence

--- com.ausy.rental.core/src/com/ausy/rental/core/RentalCoreActivator.java
+++ com.ausy.rental.core/src/com/ausy/rental/core/RentalCoreActivator.java
@@ -3,14 +3,21 @@ package com.ausy.rental.core;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+import com.opcoach.training.rental.RentalAgency;
+import com.opcoach.training.rental.core.helpers.RentalAgencyGenerator;
+
 public class RentalCoreActivator implements BundleActivator {
 
 	private static BundleContext context;
+	private static RentalAgency agency=RentalAgencyGenerator.createSampleAgency();
 
 	static BundleContext getContext() {
 		return context;
 	}
-
+	
+	public static RentalAgency getAgency() {
+		return agency;
+	}
 	/*
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
